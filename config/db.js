f2da033be62390249b9094c855218d0740df0985
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-03-21 00:04:19 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const db = await mysql.createConnection({
   host: process.env.DB_HOST, 
   user: process.env.DB_USER,       
-  password: process.env.DB_PASSWORD, 
+  password: process.env.DB_PASS, 
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
