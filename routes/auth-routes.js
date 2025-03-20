00ff4d4b9aad67d6000dfd0232a6cbfcf05849d2
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-03-20 16:26:56 +0600

    update

--- routes/auth-routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { register, login } from '../controllers/auth-controller.js';
-
-const router = express.Router();
-
-router.post('/api/auth/register', register);
-router.post('/api/auth/login', login);
-
-export default router;
