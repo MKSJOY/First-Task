d023a8cd71a1f07ae3d5297c0ff528d5bdffecfc
Mrinal Kanti Saha Joy <122686405+MKSJOY@users.noreply.github.com>
2025-03-21 00:03:39 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = await mysql.createConnection({
-  host: 'localhost', 
-  user: 'root',       
-  password: '', 
-  database: 'node_app',
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER,       
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
